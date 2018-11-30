c323183ee75878516e399afbafdad946c26b620b
FU <aderx@qq.com>
2018-11-30 15:54:47 +0800

    解决多页传参与页面跳转问题，页面加载时增加遮罩

--- static/admin/js/basic.js
+++ static/admin/js/basic.js
@@ -5,9 +5,11 @@
 //获取当前页面的URL属性值，接收参数name：属性名，如果有对应属性值则返回属性值否则返回 null
 
     (function ($) {
-        $.getUrlParam = function (name) {
-            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i")
-                ,r = window.location.search.substr(1).match(reg);
+        $.getUrlParam = function (name,site) {
+            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"),r = window.location.search.substr(1).match(reg);
+            if(site === 1){
+                r = window.parent.location.search.substr(1).match(reg)
+            }
             if (r != null) return decodeURI(r[2]);
             return null;
         };
@@ -44,6 +46,45 @@
             }
         })
     }
+
+
+jQuery.cookie = function (name, value, options) {
+    if (typeof value != 'undefined') {
+        options = options || {};
+        if (value === null) {
+            value = '';
+            options.expires = -1;
+        }
+        var expires = '';
+        if (options.expires && (typeof options.expires == 'number' || options.expires.toUTCString)) {
+            var date;
+            if (typeof options.expires == 'number') {
+                date = new Date();
+                date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
+            } else {
+                date = options.expires;
+            }
+            expires = '; expires=' + date.toUTCString();
+        }
+        var path = options.path ? '; path=' + (options.path) : '';
+        var domain = options.domain ? '; domain=' + (options.domain) : '';
+        var secure = options.secure ? '; secure' : '';
+        document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
+    } else {
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+}
 
 
 
