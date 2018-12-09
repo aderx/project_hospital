c361cef4ad40b080e72b1633d7e744d048c43fd3
FU <aderx@qq.com>
2018-12-09 17:00:43 +0800

    修复代码重构BUG

--- static/admin/js/tableNormal.js
+++ static/admin/js/tableNormal.js
@@ -1,53 +1,48 @@
-$(function(){
-    layui.use(['table','form','laydate'], function () {
+$(function () {
+    layui.use(['table', 'form', 'laydate'], function () {
         var table = layui.table
-            ,laydate = layui.laydate
-            ,addTable = {}// 初始化
-            ,date = new Date()
-            ,today = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate();//获取年-月-日
-            ,args_date= {
+            , laydate = layui.laydate// 初始化
+            , date = new Date()
+            , today = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate()//获取年-月-日
+            , args_date = {
                 value: today,
-                format:"y-M-d"
+                format: "y-M-d"
             }
-            ,args_table = {
-            elem: '#table'
-            , page: true
-            , toolbar: '#toolBar'
-            , height: 'full-25'
-            , title: '数据表'
-            , limit: 15
-            , limits:[15,25,35,45,55,65,75,85,95]
-            , minWidth:1200
-            , text:{
-                none: "暂时还没有数据哦！"
+            , args_table = {
+                elem: '#table'
+                , page: true
+                , toolbar: '#toolBar'
+                , height: 'full-25'
+                , title: '数据表'
+                , limit: 15
+                , limits: [15, 25, 35, 45, 55, 65, 75, 85, 95]
+                , minWidth: 1200
+                , text: {
+                    none: "暂时还没有数据哦！"
+                }
             }
-        }
-            ,tbs = addTable.table
-            ,das = addTable.date;
+            , tbs = addTable.table
+            , das = addTable.date;
 
 
-
-        function comp(x,y){
-            for(var name in y){
-                if(!x[name]){
+        function comp(x, y) {
+            for (var name in y) {
+                if (!x[name]) {
                     x[name] = y[name];
                 }
             }
         }
-        
-        try{
-            if(tbs.cols){
-                comp(tbs,args_table);
+
+            if (tbs.cols) {
+                comp(tbs, args_table);
                 table.render(tbs);
             }
-            if(das.elem){
-                comp(das,args_date);
+            if (das.elem) {
+                comp(das, args_date);
                 laydate.render(das);
             }
-        }catch (e) {
-            
-        }
-        
+
+
     });
 });
 
