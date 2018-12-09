ccf3e165674709f8213372be2e12ac224e2142ed
FU <aderx@qq.com>
2018-12-09 16:33:35 +0800

    JS大重构，更加便捷的操作表格，结构大变化

--- static/admin/js/basic.js
+++ static/admin/js/basic.js
@@ -145,29 +145,37 @@ var func = {//this = obj
         })
     },
     "toolFunc":function(value){
-        layui.use('table',function() {
-            var table = layui.table,layer = layui.layer;
-            table.on('tool(' + value[0] + ')', function (obj) {
-                var w = document.body.clientWidth - 20
-                    , h = document.body.clientHeight - 20;
-                var data = obj.data;//获得当前行数据
-                if (obj.event === value[1]) {//编辑
-                    layer.open({
-                        type: value[type] || 1,
-                        title: value[title] || "详情",
-                        content: value[content] || "无内容",
-                        area: [w + "px", h + "px"],
-                        resize: false,
-                        move: false
-                    });
-                }
+        for(var i=0;i<value.length;i++){
+            if(typeof value !== "object" || typeof value[0] !== "object"){
+                console.error("参数填写错误！");
+                return ;
+            }
+            var vas = value[i];
+            layui.use('table',function() {
+                var table = layui.table,layer = layui.layer,filt = vas.filter || "table1",tool = vas.tool || "tool";
+                table.on(''+tool+'(' + filt + ')', function (obj) {
+                    var w = document.body.clientWidth - 20
+                        , h = document.body.clientHeight - 20;
+                    var data = obj.data;//获得当前行数据
+                    if (obj.event === vas.event) {//弹出窗口
+                        layer.open({
+                            type: vas.type || 1,
+                            title: vas.title || "详情",
+                            content: vas.content || "无内容",
+                            area: vas.area || [w + "px", h + "px"],
+                            resize: vas.resize || false,
+                            move: vas.move || false
+                        });
+                    }
+                });
             });
-        });
+        }
+
     }
 }
 
 function tableFunc(){
-    try {
+    // try {
         var obj = arguments[0].obj;
         for(var name in arguments[0]){
             if(func[name]){
@@ -175,174 +183,9 @@ function tableFunc(){
                 console.log(name +" 已加载！");
             }
         }
-    }catch (e) {
-        console.error("程序遇到一个无法处理的错误！");
-    }
+    // }catch (e) {
+    //     console.error("程序遇到一个无法处理的错误！");
+    // }
 }
-
-//：frame通用函数
-//frame_all({"back":true},{})
-/*function frame_all(arg) {//obj：TABLE组件返回参数
-    var obj = this;
-    //判断并添加 返回 按钮
-    for (var i = 0; i < arg.length; i++) {
-        var aName = "",value="";
-        if(typeof arg[i] === "object"){
-            for(var name in arg[i]){
-                aName = name;
-                value = arg[i][name];
-            }
-        }else{
-            aName = arg[i];
-        }
-        switch (aName) {//依照不同的参数运行不同的函数
-            case "back" :
-                backPage(value);
-                break;//添加返回按钮
-            case "table":
-                tableFunc();
-                break;//表格加载完毕执行函数
-            case "link":
-                tableClick(value);
-                break;//表格点击函数
-            case "message":
-                addTableMessage(value);
-                break;//提示信息
-            case "check":
-                checkTable(value);
-                break;//检测表格
-            case "tools":
-                toolsFunc(value);
-                break;
-        }
-
-    }
-
-    //检测表格
-    function checkTable(value){
-        console.log(obj.data);
-    }//未启用
-
-    //返回按钮
-    function backPage(value) {
-        value === ""?  true : value;
-        if(!value || value === "false"){
-            console.log("当前页面已经禁止加载返回按钮了！")
-            return ;
-        }
-        if(typeof value === "object"){//value: {"link":"https://www.x.com"}
-            try{
-                value.link
-            }catch(e){
-                return ;
-            }
-        }else{
-            if (document.referrer !== 'null' || document.referrer !== "") {
-                var $tool = $(".h_tool")
-                if ($tool) {
-                    $tool.prepend("<button class=\"layui-btn\" data-type=\"backUrl\">返回</button>\n")
-                } else {
-                    $("body").prepend("<div class=\"layui-row search_button\">\n" +
-                        "    <div class=\"col-xs-12\">\n" +
-                        "        <div class=\"test-table-reload-btn\" style=\"margin-top: 20px;\">\n" +
-                        "            <button class=\"layui-btn\" data-type=\"backUrl\">返回</button>\n" +
-                        "        </div>\n" +
-                        "    </div>\n" +
-                        "</div>")
-                }
-
-            }//添加返回上一页按钮
-        }
-
-
-        //返回按钮点击事件
-        $("button[data-type='backUrl']").on('click', function () {
-            layer.confirm("确定返回表格信息吗？", function () {
-                location.href = document.referrer;//返回上一个页面
-            });
-        });
-    }
-
-    function tableFunc(){
-
-    }
-    //行单击事件
-    /!*function tableClick(value){
-        layui.use('table',function(){
-            var table = layui.table;
-            table.on('tool('+value+')', function (obj) {
-                var data = obj.data;
-                console.log(data)
-                if (obj.event === "openLink") {
-                    layer.confirm("确定跳转链接吗？", function () {
-                        if(data.link){
-                            location.href = data.link +"?link="+ encodeURIComponent(window.location.href);
-                        }else{
-                            layer.alert("没有可以跳转的链接哦！")
-                        }
-                    });
-
-                }
-            });
-        })
-    }*!/
-
-    //message提示信息 未解决问题：内容相同message会共有同列
-    /!*function addTableMessage(value) {
-        var num = value[0] || 1//添加messages的表格列数
-            ,timeIn = Number(value[1]) || 500//动画渐入时间
-            ,timeOut = Number(value[2]) || 100//动画渐出时间
-            ,oData = obj.data;
-        $("td:nth-child(" + Number(num) + ")").mouseover(function (e) {
-            if($("#h_tips").length <=0){
-                $("body").append($("<div>").attr("id","h_tips"))
-            }
-            for (var i = 0; i < oData.length; i++) {
-                for (var titles in oData[i]) {
-                    if (oData[i][titles] === $(this).text()) {//确保当前的单元值与捕获的数据中的值对应，避免对应错误
-                        $("#h_tips").html("");
-                        try {
-                            for (var j = 0; j < oData[i].messages.length; j++) {
-                                $("#h_tips").append("<p>\n" + "<span class=\"tip_title\">" + oData[i].messages[j].name + "</span>：<span class=\"tip_conts\">" + oData[i].messages[j].value + "</span>\n" + "</p>")
-                            }
-                        } catch (e) {
-                            continue;
-                        }
-                        e = e || window.event;
-                        _x = e.pageX || e.clientX + document.body.scroolLeft;
-                        _y = e.pageY || e.clientY + document.body.scrollTop;
-                        //$("#tips").fadeOut(50);
-                        $("#h_tips").css("left", _x + 'px').css("top", _y + 'px').fadeIn(timeIn);
-                        $(this).mouseleave(function () {
-                            $("#h_tips").fadeOut(timeOut).remove();
-                        })
-                        break;
-                    }
-                }
-            }
-        })
-    }*!/
-
-    function toolsFunc(value){
-        layui.use('table',function() {
-            var table = layui.table,layer = layui.layer;
-            table.on('tool(' + value[0] + ')', function (obj) {
-                var w = document.body.clientWidth - 20
-                    , h = document.body.clientHeight - 20;
-                var data = obj.data;//获得当前行数据
-                if (obj.event === value[1]) {//编辑
-                    layer.open({
-                        type: 2,
-                        title: value[2],
-                        content: value[3],
-                        area: [w + "px", h + "px"],
-                        resize: false,
-                        move: false
-                    });
-                }
-            });
-        });
-    }
-}*/
 
 
--- static/admin/js/tableNormal.js
+++ static/admin/js/tableNormal.js
@@ -1,12 +1,54 @@
-var addTable = {
-    elem: '#table'
-    , page: true //开启分页
-    , toolbar: '#toolBar'
-    , height: 'full-25'
-    , title: '数据表'
-    , minWidth: 1200
-    , text: {
-        none: "暂时还没有数据哦！"
-    }
-};
+$(function(){
+    layui.use(['table','form','laydate'], function () {
+        var table = layui.table
+            ,laydate = layui.laydate
+            ,addTable = {}// 初始化
+            ,date = new Date()
+            ,today = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate();//获取年-月-日
+            ,args_date= {
+                value: today,
+                format:"y-M-d"
+            }
+            ,args_table = {
+            elem: '#table'
+            , page: true
+            , toolbar: '#toolBar'
+            , height: 'full-25'
+            , title: '数据表'
+            , limit: 15
+            , limits:[15,25,35,45,55,65,75,85,95]
+            , minWidth:1200
+            , text:{
+                none: "暂时还没有数据哦！"
+            }
+        }
+            ,tbs = addTable.table
+            ,das = addTable.date;
+
+
+
+        function comp(x,y){
+            for(var name in y){
+                if(!x[name]){
+                    x[name] = y[name];
+                }
+            }
+        }
+        
+        try{
+            if(tbs.cols){
+                comp(tbs,args_table);
+                table.render(tbs);
+            }
+            if(das.elem){
+                comp(das,args_date);
+                laydate.render(das);
+            }
+        }catch (e) {
+            
+        }
+        
+    });
+});
+
 
