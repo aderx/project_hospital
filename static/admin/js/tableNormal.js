f43ae9a7689b39f27feca34144a42f26074f6b67
FU <aderx@qq.com>
2019-01-02 19:14:50 +0800

    合并表单表格渲染代码（暂时保留了原始JS），优化文档结构，优化基础函数库

--- static/admin/js/tableNormal.js
+++ static/admin/js/tableNormal.js
@@ -1,19 +1,14 @@
-var date = new Date()
-    , year = date.getFullYear()
-    , month = date.getMonth() + 1
-    , day = date.getDate()
-    , today = year+ "-" + month + "-" + day//获取年-月-日
-    , time = date.getTime();
 
 $(function () {
-    layui.use(['table', 'form', 'laydate','element'], function () {
-        var table = layui.table,form = layui.form, element = layui.element, laydate = layui.laydate
-            , args_date = {
+    if(addTable && Type(addTable) === "json"){
+        layui.use(['table', 'form', 'laydate','element'], function () {
+            var table = layui.table,form = layui.form, element = layui.element, laydate = layui.laydate
+                , args_date = {
                 elem: "#date",
                 value: today,
                 format: "y-M-d"
             }
-            , args_table = {
+                , args_table = {
                 elem: '#table'
                 , page: true
                 , toolbar: '#toolBar'
@@ -27,45 +22,39 @@ $(function () {
                 }
                 , id : "table"
             }
-            , tbs = addTable.table//自定义表格参数
-            , das = addTable.date//自定义日期选择器参数
-            , fos = addTable.form//自定义表单参数-未启用
-            , res = addTable.reSet;//自定义数据重载参数
+                , tbs = addTable.table//自定义表格参数
+                , das = addTable.date//自定义日期选择器参数
+                , fos = addTable.form//自定义表单参数-未启用
+                , res = addTable.reSet;//自定义数据重载参数
 
-        //判断是否有自定义数据(与默认数据相比)
-        function comp(x, y) {
-            for (var name in y) {
-                if (!x[name]) {
-                    x[name] = y[name];
-                }
-            }
-        }//匹配默认值
-
-        //默认表格渲染
-        if (Type(tbs) === "json") {
-                comp(tbs, args_table);
+            //表格渲染
+            if (Type(tbs) === "json") {
+                compereData(tbs, args_table);
                 table.render(tbs);
             }else if(Type(tbs) === "array"){
                 for(var i=0;i<tbs.length;i++){
                     if(Type(tbs[i]) === "json"){
-                        comp(tbs[i], args_table);
+                        compereData(tbs[i], args_table);
                         table.render(tbs[i]);
                     }
                 }
             }//TABLE表格创建
-        //默认日期选择器渲染
-        if (das && das !== false) {
+            //默认日期选择器渲染
+            if (das && das !== false) {
+                //日期选择为true则使用默认配置渲染
                 if(das === true){
                     a(args_date);
+                    //如果是JSON格式传入则默认使用此格式渲染
                 }else if(Type(das) === "json"){
                     a(das);
+                    //如果传入为数组则多次渲染
                 }else if(Type(das) === "array"){
                     for(var j=0;j<das.length;j++){
                         a(das[j]);
                     }
                 }
                 function a(das){
-                    comp(das, args_date);
+                    compereData(das, args_date);
                     if(das.range){
                         var bars = "-";
                         if(Type(das.range) === "array"){
@@ -76,31 +65,33 @@ $(function () {
                     laydate.render(das);
                 }
             }//DATE创建
-        //表格数据重载(查询数据)
-        if(res){
-            var $ = layui.$,type = res.type || "search", active = {},value = {};
-            active[type] = function(){
-                for(var name in res.where){
-                    value[name] = $(res.where[name]).val();
+
+            //表格数据重载(查询数据)
+            if(res){
+                var $ = layui.$,type = res.type || "search", active = {},value = {};
+                active[type] = function(){
+                    for(var name in res.where){
+                        value[name] = $(res.where[name]).val();
+                    }
+                    //执行重载
+                    table.reload(res.tid || args_table.id, {
+                        url:res.url
+                        ,where: res.where || {}
+                    });
+                    //重新绑定事件
+                    $(".layui-table-tool .layui-btn").on('click',function(){
+                        var type = $(this).data('type');
+                        active[type] ? active[type].call(this) : '';
+                    });
                 }
-                //执行重载
-                table.reload(res.tid || args_table.id, {
-                    url:res.url
-                    ,where: res.where || {}
-                });
-                //重新绑定事件
+                //首次页面渲染后按钮事件绑定
                 $(".layui-table-tool .layui-btn").on('click',function(){
                     var type = $(this).data('type');
                     active[type] ? active[type].call(this) : '';
                 });
             }
-            //首次页面渲染后按钮事件绑定
-            $(".layui-table-tool .layui-btn").on('click',function(){
-                var type = $(this).data('type');
-                active[type] ? active[type].call(this) : '';
-            });
-        }
-    });
+        });
+    }
 });
 
 
